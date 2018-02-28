8e2857ba79ea567c0566a148c363caa45603c625
andrew <andrew@miscellanea.com>
2018-02-28 13:21:11 +1100

    text-array example

---
+++ tips_and_tricks/text/text-array/sketch.js
@@ -0,0 +1,50 @@
+// a variable is created her to hold our font
+// by placing the variable definition here, it is available to the entire sketch
+var miniver;
+
+// a variable to contain an array (a list) of each letter in my chgosen text
+var myTextAsLetters;
+
+// a variable we can use to count through the letters and display them
+var count = 0;
+
+// preload happens before setup and draw
+// making sure our font will be loaded and ready when we need it
+function preload() {
+  //font can be a ttf or otf
+  miniver = loadFont('assets/Miniver-Regular.ttf');
+}
+
+function setup() {
+  createCanvas(800,400);
+  background(0);
+  
+  // a variable to hold my full text
+  var myText = 'All of my text as individual characters ';
+  // split the text into individual letters.
+  // see https://p5js.org/reference/#/p5/split
+  myTextAsLetters = split(myText, '');
+  // print the letters out to the console just so we can see what an array looks like
+  print(myTextAsLetters);
+  
+  // setup the parameters for the text we will use
+  textFont(miniver);
+  textAlign(CENTER,BASELINE);
+  fill(200);
+  textSize(40);
+}
+
+function draw() {
+  background(50);
+  
+  // draw each letter to the screen one at a time. 
+  // we access the letters through square brackets
+  // the first element in the array is [0], the second element is [1] etc.
+  // floor() rounds values down to the nearest whole number 
+  // so that we can slow everything down adding a small fraction to count each frame. 
+  text(myTextAsLetters[floor(count)], width/2,height/2);
+  count = count+0.05;
+  if(floor(count) == myTextAsLetters.length){
+    count = 0;
+  }
+}
