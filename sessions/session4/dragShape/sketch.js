f49641e066bf34689a4a672473347a54c6e58431
Will McLean <willmcl@gmail.com>
2020-03-24 18:05:58 +1100

    Add a drag shape exercise.

---
+++ sessions/session4/dragShape/sketch.js
@@ -0,0 +1,38 @@
+let grabbed = false;
+let shapeX;
+let shapeY;
+const radius = 50;
+const diameter = radius*2;
+
+function setup() {
+  // put setup code here
+  createCanvas(500,500);
+  background(66, 135, 245);
+  
+  shapeX = width/2;
+  shapeY = height/2;
+}
+
+function draw () {
+  background(66, 135, 245);
+  ellipse(shapeX, shapeY, diameter, diameter);
+}
+
+function mousePressed () {
+  let d = dist(mouseX, mouseY, shapeX, shapeY);
+  if( d < radius) {
+    grabbed = true;
+  } else {
+    grabbed = false;
+  }
+}
+function mouseReleased () {
+  grabbed = false;
+}
+
+function mouseDragged() {
+  if(grabbed){
+    shapeX = mouseX;
+    shapeY = mouseY;
+  }
+}
