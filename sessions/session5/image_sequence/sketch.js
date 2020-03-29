d47324da94a08b86a485ce2f87bcd2c066b35891
Will McLean <willmcl@gmail.com>
2020-03-30 09:30:20 +1100

    Small change to the image sequence exercise.

--- sessions/session5/image_sequence/sketch.js
+++ sessions/session5/image_sequence/sketch.js
@@ -1,5 +1,6 @@
 let images;
 let imageIndex = 0;
+
 function preload(){
   images = [
     loadImage('assets/star_1.png'),
@@ -16,34 +17,36 @@ function preload(){
 let cnv;
 function setup() {
   cnv = createCanvas(600,600);
-  // // (3.) Attach listener for activity on canvas only
-  // cnv.mouseMoved(mouseMovesFrame);
-  frameRate(3);
+   // (3.) Attach listener for activity on canvas only
+   cnv.mouseMoved(mouseMovesFrame);
+  
+//  // (3.) set frame rate for animate on draw
+//  frameRate(10);
 }
 
 function draw () {
   background(240);
   
   // 1. Move frame on every draw
-  moveFrame();
+  //  moveFrame();
   
   image( images[imageIndex], 0, 0, 600, 600 );
 }
 
-function moveFrame() {
-  imageIndex++;
-  if( imageIndex > images.length - 1 ) {
-    imageIndex = 0;
-  }
-}
-
+//function moveFrame() {
+//  imageIndex++;
+//  if( imageIndex > images.length - 1 ) {
+//    imageIndex = 0;
+//  }
+//}
+//
 //// 2. Move animation based on click
 //function mouseClicked() {
 //  moveFrame();
 //}
 
-//// 3. Move animation based on mouseX position
-//function mouseMovesFrame() {
-//  // Map the mouse position to the number of images.
-//  imageIndex = map(mouseX, 0, 600, 0, images.length - 1).toFixed(0);
-//}
+// 3. Move animation based on mouseX position
+function mouseMovesFrame() {
+  // Map the mouse position to the number of images.
+  imageIndex = Math.max( 0, map(mouseX, 0, 600, 0, images.length - 1).toFixed(0) );
+}
