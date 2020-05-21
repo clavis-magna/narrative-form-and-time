2d4e710cbde578bcc60b5e75e6da1fa5f8e1a5a5
Will McLean <willmcl@gmail.com>
2020-05-22 08:24:09 +1000

    Update the audio exercise code to address common issues in student projects.

--- sessions/session4/audio/sketch.js
+++ sessions/session4/audio/sketch.js
@@ -1,9 +1,15 @@
 var screen = 1;
-var numberOfPages = 3;
+var numberOfPages = 5;
 
-let mySound;
+let crowd;
+let fire;
+let river;
+let trainCrossing;
 function preload(){
-  mySound = loadSound('crowd-talking-1.mp3');
+  crowd = loadSound('crowd-talking-1.mp3');
+  fire = loadSound('fire-1.mp3');
+  river = loadSound('river-2.mp3');
+  trainCrossing = loadSound('train-crossing-bell-01.mp3');
 }
 
 function setup() {
@@ -16,30 +22,48 @@ function draw () {
   background(66, 135, 245);
  if (screen==1) {
    //draw first screen
-   text("Page 1", width/2,height/2);
-   
-  }
-  else if (screen==2) {
+   text("Page 1 (No sound playing)", width/2,height/2);
+  } else if (screen==2) {
     //draw third screen
-    text("Page 2 (sound should be playing)", width/2,height/2);
-  }
-  
-  else if (screen==3) {
+    text("Page 2 (Crowd sound should be playing)", width/2,height/2);
+  } else if (screen==3) {
     //draw fourth screen
-    text("Page 3", width/2,height/2);
+    text("Page 3 (Fire sound should be playing)", width/2,height/2);
+  } else if (screen==4) {
+    //draw fourth screen
+    text("Page 4 (River sound should be playing)", width/2,height/2);
+  } else if (screen==5) {
+    //draw fourth screen
+    text("Page 5 (Train crossing sound should be playing)", width/2,height/2);
   }
   
 }
 
-function mousePressed () {
+function incrementScreen () {
   screen++;
   if(screen > numberOfPages){
     screen = 1;
   }
-  
-  if ( screen == 2 ) {
-    mySound.play();
-  } else {
-    mySound.stop();
+}
+
+function mousePressed () {
+  if ( screen == 1 ) {
+    crowd.play();
+    incrementScreen();
+  } else if ( screen == 2 ) {
+    crowd.stop();
+    fire.play();
+    incrementScreen();
+  } else if ( screen == 3 ) {
+    fire.stop();
+    river.play();
+    incrementScreen();
+  } else if ( screen == 4 ) {
+    river.stop();
+    trainCrossing.play();
+    incrementScreen();
+  } else if ( screen == 5 ) {
+    trainCrossing.stop();
+    incrementScreen();
   }
 }
